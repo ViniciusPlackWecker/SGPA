b9936561967b10fa2bc897a986ffd734b8c0c813
Vinicius Wecker <viniciuswecker@ienh.com.br>
2024-07-06 18:20:04 -0300

    Ajustes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    
+                    {{ __('Este é um sistema personalizado para gestão de projetos acadêmicos. Aqui você pode visualizar, enviar e gerenciar seus projetos de forma eficiente.') }}
+                    <p>Estamos aqui para ajudar você a organizar suas atividades acadêmicas de maneira simples e eficaz. Explore as funcionalidades disponíveis e aproveite ao máximo sua experiência!</p>
+                    <p>Se precisar de ajuda ou tiver alguma dúvida, não hesite em entrar em contato conosco.</p>
                 </div>
             </div>
         </div>
