f6add415c9757dbb813db9698e78bd43f4a5f164
Vinicius Wecker <viniciuswecker@ienh.com.br>
2024-07-06 14:37:27 -0300

    Ajustes

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -21,7 +21,6 @@
                         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                             <div class="p-6 border-b border-gray-200 dark:border-gray-700">
                                 <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-200 mb-2">{{ $file->old_name }}</h3>
-                                <strong class="text-white">Assunto:</strong> <span class="text-white">{{ $file->topic }}</span>
                                 <p class="text-sm text-gray-600 dark:text-gray-400 mb-4">Data de Envio: {{ $file->created_at->format('d/m/Y H:i:s') }}</p>
                                 <div class="flex justify-end">
                                     <a href="{{ route('project.download', ['id' => $file->id]) }}" class="border bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
